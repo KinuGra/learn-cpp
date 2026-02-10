c763cce13194012bf7a61fd6cc5fae2cc9e710a1
gear_kinu <techgit1024@gmail.com>
2026-02-10 16:48:44 +0900

    C++ L1-L2の勉強
    cout

---
+++ easyCpp/L1-2/Ex.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 123 << "\n"
+         << "\\100もらった\n"
+         << "またあした\n";
+
+    cout << 06 << "\n"
+         << 024 << "\n"
+         << 015 << "\n";
+    cout << 0x6 << "\n"
+         << 0x14 << "\n"
+         << 0xd << "\n";
+}
