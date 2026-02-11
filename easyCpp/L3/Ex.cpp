94879123b5e6c75d10fc9c8961a3d421fa5f8717
gear_kinu <techgit1024@gmail.com>
2026-02-11 13:42:04 +0900

    L3 変数

---
+++ easyCpp/L3/Ex.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double h, w;
+    cout << "身長と体重を入力してください。\n";
+    cin >> h >> w;
+    cout << "身長：" << h << '\n';
+    cout << "体重：" << w << '\n';
+}
