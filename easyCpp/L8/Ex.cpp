c814cf04279979ea9827d51f4e1d2a03040492c5
gear_kinu <techgit1024@gmail.com>
2026-03-16 20:36:37 +0900

    L8 ポインタ

---
+++ easyCpp/L8/Ex.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void add(int *x1, int *x2, int a)
+{
+    *x1 += a;
+    *x2 += a;
+}
+
+int main()
+{
+    int x1, x2, a;
+    std::cout << "2科目の点数を入力してください。\n";
+    std::cin >> x1 >> x2;
+    std::cout << "加算する点数を入力してください。\n";
+    std::cin >> a;
+    std::cout << a << "点加算しましたので\n";
+    add(&x1, &x2, a);
+    std::cout << "科目1は" << x1 << "点となりました。\n";
+    std::cout << "科目2は" << x2 << "点となりました。\n";
+}
